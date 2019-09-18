3eddc1ca7d70e8913af82e7d123a2e6869561848
tanshio <rakuburomail+tanshio@gmail.com>
2019-09-19 03:33:32 +0900

    feat: immer

--- src/store/counter/reducers.ts
+++ src/store/counter/reducers.ts
@@ -1,3 +1,4 @@
+import produce from 'immer'
 import { CounterActionTypes } from './actions'
 import { DECREMENT_COUNT, INCREMENT_COUNT } from './actions'
 
@@ -5,16 +6,17 @@ type State = {
   count: number
 }
 
-export const counter = (
-  state: State = { count: 0 },
-  action: CounterActionTypes
-): State => {
-  switch (action.type) {
-    case INCREMENT_COUNT:
-      return { count: state.count + action.payload }
-    case DECREMENT_COUNT:
-      return { count: state.count - action.payload }
-    default:
-      return state
+export const counter = produce(
+  (draft: State = { count: 0 }, action: CounterActionTypes): State => {
+    switch (action.type) {
+      case INCREMENT_COUNT:
+        draft.count = draft.count + action.payload
+        return draft
+      case DECREMENT_COUNT:
+        draft.count = draft.count - action.payload
+        return draft
+      default:
+        return draft
+    }
   }
-}
+)
