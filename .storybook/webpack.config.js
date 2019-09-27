e6f6cd779efa6439b96f72489d28464e45d845db
tanshio <rakuburomail+tanshio@gmail.com>
2019-09-27 18:08:10 +0900

    WIP storybook plugins

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,24 +1,34 @@
 module.exports = ({ config, mode }) => {
   config.module.rules.push({
     test: /\.(ts|tsx)$/,
-    loader: require.resolve('babel-loader'),
-    options: {
-      presets: [['react-app', { flow: false, typescript: true }]],
-    },
-  });
-
-  config.module.rules.push({
-    test: /\.(ts|tsx)$/,
     use: [
+      // require.resolve('ts-loader'),
       {
         loader: require.resolve('react-docgen-typescript-loader'),
       },
+      {
+        loader: require.resolve('@storybook/addon-storysource/loader'),
+        options: { parser: 'typescript' },
+      },
+      {
+        loader: require.resolve('babel-loader'),
+        options: {
+          presets: [['react-app', { flow: false, typescript: true }]],
+        },
+      },
+      // {
+      //   loader: require.resolve('react-docgen-typescript-loader'),
+      // },
+      // {
+      //   loader: require.resolve('@storybook/addon-storysource/loader'),
+      //   options: { parser: 'typescript' }
+      // }
     ],
-  });
+  })
 
-  config.resolve.extensions.push('.ts', '.tsx');
-  return config;
-};
+  config.resolve.extensions.push('.ts', '.tsx')
+  return config
+}
 
 // module.exports = ({ config }) => {
 //   config.module.rules.push({
--- src/components/parts/Counter/index.stories.tsx
+++ src/components/parts/Counter/index.stories.tsx
@@ -1,12 +1,28 @@
 import * as React from 'react'
 import { storiesOf } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
+import { withInfo } from '@storybook/addon-info'
+import { withKnobs } from '@storybook/addon-knobs'
 import { Counter } from './'
 
-const stories = storiesOf('Components', module)
+// const stories = storiesOf('Components', module)
 
-stories.add(
-  'Counter',
-  () => <Counter counter={0} increment={action('increment')} />,
-  { info: { inline: true } }
+// stories
+//   .addDecorator(withKnobs)
+//   .addDecorator(withInfo({ inline: true }))
+//   .add('Counter', () => <Counter counter={0} increment={action('increment')} />)
+
+storiesOf('Component', module).add(
+  'simple info',
+  withInfo({
+    styles: {
+      header: {
+        h1: {
+          color: 'red',
+        },
+      },
+    },
+    text: 'String or React Element with docs about my component', // Warning! This option's name will be likely renamed to "summary" in 3.3 release. Follow this PR #1501 for details
+    // other possible options see in Global options section below
+  })(() => <Counter counter={0} increment={action('increment')} />)
 )
