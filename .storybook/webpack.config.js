c223cbffd3cd76cc83edcf4d6c980a8081e780b2
tanshio <rakuburomail+tanshio@gmail.com>
2019-10-14 05:24:24 +0900

    chore: storybook settings

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,49 +1,28 @@
 module.exports = ({ config, mode }) => {
   config.module.rules.push({
-    test: /\.(js|ts|tsx)$/,
+    test: /\.(js|jsx|ts|tsx)$/,
+    use: [
+      {
+        options: {
+          presets: [
+            '@babel/preset-env',
+            '@babel/preset-react',
+            '@babel/preset-typescript',
+          ],
+        },
+        loader: require.resolve('babel-loader'),
+      },
+    ],
+  })
+  config.module.rules.push({
+    test: /\.(js|jsx|ts|tsx)$/,
     exclude: /node_modules/,
     use: [
       {
-        loader: require.resolve('babel-loader'),
-        options: {
-          presets: [['react-app', { flow: false, typescript: true }]],
-        },
-      },
-      // require.resolve('ts-loader'),
-      {
         loader: require.resolve('react-docgen-typescript-loader'),
       },
-      // {
-      //   loader: require.resolve('@storybook/addon-storysource/loader'),
-      //   options: { parser: 'typescript' },
-      // },
-      // {
-      //   loader: require.resolve('react-docgen-typescript-loader'),
-      // },
-      // {
-      //   loader: require.resolve('@storybook/addon-storysource/loader'),
-      //   options: { parser: 'typescript' }
-      // }
     ],
   })
-
   config.resolve.extensions.push('.ts', '.tsx')
   return config
 }
-
-// module.exports = ({ config }) => {
-//   config.module.rules.push({
-//     test: /\.(ts|tsx)$/,
-//     use: [
-//       {
-//         loader: require.resolve('ts-loader'),
-//       },
-//       // Optional
-//       {
-//         loader: require.resolve('react-docgen-typescript-loader'),
-//       },
-//     ],
-//   });
-//   config.resolve.extensions.push('.ts', '.tsx');
-//   return config;
-// };
